570397d49e712132e568a9547f9d4fdcc204f38c
AlexPokatilov <sasha.pokatilov@gmail.com>
2023-12-21 21:03:30 +0200

    check new format

--- main.go
+++ main.go
@@ -80,19 +80,24 @@ func formatMessage(payload WebhookPayload) string {
     resource := payload.EventData.Resources[0] // We use the first resource
     repo := payload.EventData.Repository
 
-    harborHost := getBaseURL(resource.ResourceURL)
+    parsedURL, err := url.Parse(resource.ResourceURL)
+    if err != nil {
+        log.Printf("Error parsing resource URL: %s", err)
+        return "Error formatting message: Invalid resource URL"
+    }
+    harborHost := parsedURL.Hostname()
 
     var message string
     switch payload.Type {
     case "PUSH_ARTIFACT":
         message = fmt.Sprintf("New image pushed by: <b>%s</b>\n", payload.Operator)
-        message += fmt.Sprintf("• Host: %s (https://%s/harbor/projects)\n", harborHost, harborHost)
+        message += fmt.Sprintf("• Host: %s\n", harborHost)
         message += fmt.Sprintf("• Project: <b>%s</b>\n", repo.Namespace)
         message += fmt.Sprintf("• Repository: <b>%s</b>\n", repo.RepoFullName)
         message += fmt.Sprintf("• Tag: <b>%s</b>", resource.Tag)
     case "UPLOAD_CHART":
         message = fmt.Sprintf("New chart version uploaded by: <b>%s</b>\n", payload.Operator)
-        message += fmt.Sprintf("• Host: %s (https://%s/harbor/projects)\n", harborHost, harborHost)
+        message += fmt.Sprintf("• Host: %s\n", harborHost)
         message += fmt.Sprintf("• Project: <b>%s</b>\n", repo.Namespace)
         message += fmt.Sprintf("• Chart: <b>%s</b>\n", repo.Name)
         message += fmt.Sprintf("• Version: <b>%s</b>", resource.Tag)
