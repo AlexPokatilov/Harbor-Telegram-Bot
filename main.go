9bd80280900226d5adf3cf293aefe0030bc049f2
AlexPokatilov <sasha.pokatilov@gmail.com>
2023-12-21 21:08:42 +0200

    Revert "check"
    This reverts commit 894f6f44a1b17f422cf595bdb6ba21b586d8c28e.

--- main.go
+++ main.go
@@ -85,13 +85,13 @@ func formatMessage(payload WebhookPayload) string {
     switch payload.Type {
     case "PUSH_ARTIFACT":
         message = fmt.Sprintf("New image pushed by: <b>%s</b>\n", payload.Operator)
-        message += fmt.Sprintf("• Host: %s (https://%s/harbor/projects)\n", harborHost, harborHost)
+        message += fmt.Sprintf("• Host: <a href=\"%s\">https://%s/harbor/projects</a>\n", harborHost, harborHost)
         message += fmt.Sprintf("• Project: <b>%s</b>\n", repo.Namespace)
         message += fmt.Sprintf("• Repository: <b>%s</b>\n", repo.RepoFullName)
         message += fmt.Sprintf("• Tag: <b>%s</b>", resource.Tag)
     case "UPLOAD_CHART":
         message = fmt.Sprintf("New chart version uploaded by: <b>%s</b>\n", payload.Operator)
-        message += fmt.Sprintf("• Host: %s (https://%s/harbor/projects)\n", harborHost, harborHost)
+        message += fmt.Sprintf("• Host: <a href=\"%s\">https://%s/harbor/projects</a>\n", harborHost, harborHost)
         message += fmt.Sprintf("• Project: <b>%s</b>\n", repo.Namespace)
         message += fmt.Sprintf("• Chart: <b>%s</b>\n", repo.Name)
         message += fmt.Sprintf("• Version: <b>%s</b>", resource.Tag)
